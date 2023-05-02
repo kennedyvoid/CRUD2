acd969caee20374bdc394aab7c4c7085811fe182
Kennedy Santos da Silva <75652821+kennedyvoid@users.noreply.github.com>
2023-05-01 21:16:18 -0300

    Add files via upload

---
+++ DAL/Persistence/Conexao.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL.Persistence
+{
+    public class Conexao
+    {
+       
+
+        protected SqlConnection Con; 
+        protected SqlCommand Cmd;    
+        protected SqlDataReader Dr;  
+                                     
+        
+
+        protected void AbrirConexao()
+        {
+            try
+            {
+                //connection string
+                Con = new SqlConnection("Data Source=DESKTOP-PEDTC2B;Initial Catalog=master;Integrated Security=True");
+                Con.Open();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        //MÉTODOS - FECHAR CONEXÃO:
+
+        protected void fecharConexao()
+        {
+            try
+            {
+                Con.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ DAL/dbo.Pista.sql
@@ -0,0 +1,7 @@
+﻿CREATE Table [dbo].[Pista]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Nome] NVARCHAR(50) NOT NULL, 
+    [Senha] INT NOT NULL, 
+    [Email] NVARCHAR(50) NOT NULL
+)
